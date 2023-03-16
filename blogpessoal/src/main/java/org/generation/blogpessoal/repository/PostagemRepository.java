0c6a65db5287e1a72fc2679d1860e2717ec204e0
Luan <ss.luan21@gmail.com>
2023-03-16 16:24:34 -0300

    Projeto Blog pessoal criação da tb_temas, Interface TemaRepository, vlasse TemaController

--- blogpessoal/src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -2,6 +2,7 @@
 package org.generation.blogpessoal.repository;
 
 import org.generation.blogpessoal.model.Postagem;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,7 @@ import java.util.List;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	
 	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+	
 }
